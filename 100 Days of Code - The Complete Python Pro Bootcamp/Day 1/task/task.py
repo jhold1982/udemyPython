4d3271da95b253abb517c57ae57a6301534cb776
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-06-03 12:56:42 -0500

    Completed band name generator

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,1 +1,4 @@
-print("My name is" + " " + "Angela")
+print("Welcome to the Band Name Generator.")
+cityName = input("What's the name of the city you grew up in?\n")
+petName = input("What's the name of your favorite pet?\n")
+print("Your band name is " + cityName + " " + petName)
